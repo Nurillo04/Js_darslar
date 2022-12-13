163c1b96e2ad5e8911b014fa51d3e3b9ca8e7c87
Nurillo04 <aminjonovnurillo95@gmail.com>
2022-12-13 23:13:44 +0500

    ddd

---
+++ index.js
@@ -0,0 +1,39 @@
+// var a = 10;
+// var b = 5;
+// console.log(a + b);
+
+// var a = 25;
+// var b = 15;
+// var c = a + b;
+// console.log(c);
+
+// var a = 10;
+// var b = 3;
+// var c = a + b;
+// console.log(c);
+
+// var a = 19;
+// var b = 4;
+// var c = a % b;
+// console.log(c);
+
+// var a = 10;
+// var b = 3;
+// var c = a % b;
+// console.log(c);
+
+// var a = 2;
+// var b = 10;
+// var c = a ** b;
+// console.log(c);
+var d = prompt('pulingizni kiriting');
+var a = 500;
+var b = 250;
+var dollor = 10650.34;
+
+var c = 120;
+var yevro = 10650.03;
+
+var sum = (a * dollor) + (b * dollor) + (c * yevro);
+console.log(d);
+console.log(sum);
